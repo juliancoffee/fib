c81401be410a13d40f98433c2655df70632825e0
juliancoffee <lightdarkdaughter@gmail.com>
2019-06-10 13:39:02 +0300

    fixed comment in run.sh

--- naive/run.sh
+++ naive/run.sh
@@ -6,7 +6,7 @@ printf "\n Go $1\n"
 time ./fibgo $1
 printf "\n Rust $1\n"
 time ./fibrs $1
-printf "\n Python $1\n"
+printf "\n Python (pypy) $1\n"
 time pypy3 fib.py $1
 printf "\n Julia $1\n"
 time julia fib.jl $1
