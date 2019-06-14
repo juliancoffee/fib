780dfe6db360a6aec34a280c959609f14751504e
juliancoffee <lightdarkdaughter@gmail.com>
2019-06-14 13:10:04 +0300

    create src dir

--- naive/run.sh
+++ naive/run.sh
@@ -7,11 +7,11 @@ time ./fibgo $1
 printf "\n Rust $1\n"
 time ./fibrs $1
 printf "\n Python (pypy) $1\n"
-time pypy3 fib.py $1
+time pypy3 src/fib.py $1
 printf "\n Julia $1\n"
-time julia fib.jl $1
+time julia src/fib.jl $1
 printf "\n JS $1\n"
-time node ./fib.js $1
+time node src/fib.js $1
 printf "\n Java $1\n"
 time java fib $1
 printf "\n Nim $1\n"
