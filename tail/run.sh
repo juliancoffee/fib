4c8b6ca7d85e128f152d0b4260276500bc0177b1
juliancoffee <lightdarkdaughter@gmail.com>
2019-06-15 13:17:09 +0300

    fixed run.sh to python tail

--- tail/run.sh
+++ tail/run.sh
@@ -2,3 +2,7 @@
 
 printf "\n Sheme tail $1\n"
 time echo "$1" | ./fib_tail_scm
+printf "\n Python tail $1\n"
+time python3 src/fib_tail.py $1
+printf "\n Python3 (pypy) tail $1\n"
+time pypy3 src/fib_tail.py $1
