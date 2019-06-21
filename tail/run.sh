443831639b62a9484acc16fd49bae166305bfd5f
juliancoffee <lightdarkdaughter@gmail.com>
2019-06-21 18:54:17 +0300

    added tail-recursive in clojure

--- tail/run.sh
+++ tail/run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-printf "\n Go tail $1\n"
-time ./fib_tail_go $1
+#printf "\n Go tail $1\n"
+#time ./fib_tail_go $1
 #printf "\n Sheme tail $1\n"
 #time echo "$1" | ./fib_tail_scm
+printf "\n Clojure tail $1\n"
+time echo "$1" | clojure src/fib_tail.clj
 printf "\n Haskell tail $1\n"
 time ./fib_tail_hs $1
 #printf "\n Python tail $1\n"
---
+++ tail/src/fib_tail.clj
@@ -0,0 +1,9 @@
+(defn fib [n] 
+  (defn fib_tail [n _second _first]
+    (if (< n 3)
+      (+ _second _first)
+      (recur (- n 1) _first (+ _second _first))))
+  (fib_tail n (bigint 0) (bigint 1)))
+
+(println "res = "
+         (fib (read-string (read-line))))
