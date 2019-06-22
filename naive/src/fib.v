517bef296ff14f72cfd3dd2e910ee48a30f79b98
juliancoffee <lightdarkdaughter@gmail.com>
2019-06-22 23:52:53 +0300

    V now can int64

--- naive/src/fib.v
+++ naive/src/fib.v
@@ -1,15 +1,16 @@
 import os
 
-fn fib(n int) int {
-    if n < 3 {
-        return 1
+fn fib(n i64) i64 {
+    if n < i64(3) {
+        return i64(1)
     }
-    return fib(n - 1) + fib(n - 2)
+    return fib(n - i64(1)) + fib(n - i64(2))
 }
 
 fn main() {
     num := os.args[1]
-    n := num.to_i()
+    i_n := num.to_i()
+    n := i64(i_n)
     res := fib(n)
     println(res)
 }
